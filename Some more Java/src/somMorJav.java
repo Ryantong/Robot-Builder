43224389526b7d4d8ebf032e3575d0aa3022ec3c
mhs <mhs@MHS-Science3-30>
2017-10-25 17:13:38 -0700

    hbgu

---
+++ Some more Java/src/somMorJav.java
@@ -0,0 +1,12 @@
+
+public class somMorJav {
+	public static void main(String[] args){
+	}
+	public double max(double first, double second){
+		if(first > second){
+			return first;
+		}else{
+			return second;
+		}
+	}
+}
